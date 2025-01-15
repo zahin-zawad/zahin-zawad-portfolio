632e3e998c3a2544ee2adc779769005b18de652d
Zahin Zawad <zahin.zawad.19@gmail.com>
2025-01-16 00:23:26 +0600

    Contact section done

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div id='contact' className='min-h-[70vh] min-w-full'>
+      <div className='flex flex-col items-center justify-center gap-3 space-y-6 p-14'>
+        <h1 className='text-center text-5xl md:text-7xl bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent'>Get in Touch</h1>
+
+        <p className='text-center text-lg font-semibold text-gray-500'>Want to chat? Send me an E-mail through this button and I'll respond whenever I can.</p>
+
+        <a href="mailto:zahin.zawad.19@gmail.com" className='text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600'>Contact Me</a>
+      </div>
+    </div>
   )
 }
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import image1 from '@/public/1.png'
+import image2 from '@/public/2.png'
+import image3 from '@/public/3.png'
+import image4 from '@/public/4.png'
+import image5 from '@/public/5.png'
+import {motion} from 'motion/react'
+
+
+const projectData = [
+  {
+    image: image1,
+    tittle: "Habit Tracker",
+    description: "A simple habit tracker app built with React and Tailwind CSS. It allows users to add habits and track their progress.",
+    technologies: ["React", "Tailwind CSS"],
+    github: "#",
+    live: "#"
+  },
+
+  {
+    image: image2,
+    tittle: "Todo App",
+    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    technologies: ["TypeScript","Next.js","React", "Tailwind CSS"],
+    github: "#",
+    live: "#"
+  },
+
+  {
+    image: image3,
+    tittle: "Social Media App",
+    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    technologies: ["React", "Tailwind CSS", "TypeScript"],
+    github: "#",
+    live: "#"
+  },
+
+  {
+    image: image4,
+    tittle: "Restaurant Website",
+    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    technologies: ["React", "Tailwind CSS"],
+    github: "#",
+    live: "#"
+  },
+
+  {
+    image: image5,
+    tittle: "Admin Dashboard",
+    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    technologies: ["React", "Tailwind CSS"],
+    github: "#",
+    live: "#"
+  },
+]
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div id='projects'>Projects</div>
   )
 }
