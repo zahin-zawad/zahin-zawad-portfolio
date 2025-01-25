44a987d4f18a63ee58c84b04af013ededd0de58c
Zahin Zawad <zahin.zawad.19@gmail.com>
2025-01-25 18:40:20 +0600

    updated the links

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,40 +1,58 @@
 "use client";
 
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 import heroImage from "@/public/photo.jpg";
-import { motion } from "motion/react"
-
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
-    <div id='home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
-      <div className='text-white flex flex-col justify-center items-center'>
-
+    <div
+      id="home"
+      className="px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32"
+    >
+      <div className="text-white flex flex-col justify-center items-center">
         <motion.div
-         initial={{y: -50, opacity: 0}}
-         animate={{y: 0, opacity: 1}}
-         transition={{duration: .8, delay: .2}}
-
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
         >
-          <Image 
-            src={heroImage} 
-            alt='hero' 
-            className='w-[300px] cursor-pointer rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2xl hover:shadow-indigo-600 md:w-[350px]' 
+          <Image
+            src={heroImage}
+            alt="hero"
+            className="w-[300px] cursor-pointer rounded-full shadow-xl shadow-indigo-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2xl hover:shadow-indigo-600 md:w-[350px]"
           />
         </motion.div>
 
         <motion.div
-          initial={{y: 50, opacity: 0}}
-          animate={{y:0, opacity: 1}}
-          transition= {{duration: .8, delay: .2}}
-        className='max-w-[600px] flex flex-col items-center justify-center gap-3 text-center'>
-          <h1 className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light md:text-7xl mt-10'>Zahin Zawad</h1>
-          <h3 className='bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent text-2xl font-light md:text-3xl'>Web Developer</h3>
-          <p className='md:text-base text-pretty text-sm text-gray-400'>
-            I am a web developer skilled in React, Next.js, and Tailwind, focused on building clean, scalable applications. I am highly dedicated to coding responsive websites and high-converting landing pages. I love exploring new ideas and making them happen in real life. 
-            <br/>Let's build something great together!
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className="max-w-[600px] flex flex-col items-center justify-center gap-3 text-center"
+        >
+          <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light md:text-7xl mt-10">
+            Zahin Zawad
+          </h1>
+          <h3 className="bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent text-2xl font-light md:text-3xl">
+            Web Developer
+          </h3>
+          <p className="md:text-base text-pretty text-sm text-gray-400">
+            I am a web developer skilled in React, Next.js, and Tailwind,
+            focused on building clean, scalable applications. I am highly
+            dedicated to coding responsive websites and high-converting landing
+            pages. I love exploring new ideas and making them happen in real
+            life.
+            <br />
+            Let's build something great together!
           </p>
+
+          <a
+            href="https://docs.google.com/document/d/15AL-vlvWuO6XFr7kuAT7t1dXDrHwMh1Tj-oiOAnv3vM/edit?usp=sharing"
+            target="_blank"
+            className="mt-10 text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
+          >
+            Download Resume
+          </a>
         </motion.div>
       </div>
     </div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,60 +1,93 @@
-import React from 'react'
-import image1 from '@/public/1.png'
-import image2 from '@/public/2.png'
-import image3 from '@/public/3.png'
-import image4 from '@/public/4.png'
-import image5 from '@/public/5.png'
-import {motion} from 'motion/react'
-import Image, { StaticImageData } from 'next/image'
-
+import React from "react";
+import image1 from "@/public/1.png";
+import image2 from "@/public/2.png";
+import image3 from "@/public/3.png";
+import image4 from "@/public/4.png";
+import image5 from "@/public/5.png";
+import image6 from "@/public/6.png";
+import image7 from "@/public/7.png";
+import Image, { StaticImageData } from "next/image";
 
 const projectData = [
   {
     image: image1,
-    title: "Habit Tracker",
-    description: "A simple habit tracker app built with React and Tailwind CSS. It allows users to add habits and track their progress.",
-    technologies: ["React", "Tailwind CSS"],
-    github: "#",
-    live: "#"
+    title: "Coffee Shop Website",
+    description:
+      "Complete Responsive Coffee Shop Website Using ReactJS, Tailwind CSS & Framer Motion",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Framer Motion",
+    ],
+    github: "https://github.com/zahin-zawad/zawads-cafe",
+    live: "https://zawads-cafe-o2hl.vercel.app/",
   },
 
   {
     image: image2,
-    title: "Todo App",
-    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
-    technologies: ["TypeScript","Next.js","React", "Tailwind CSS"],
-    github: "#",
-    live: "#"
+    title: "Restaurant Website",
+    description:
+      "Modern Animated Restaurant Website using ReactJS + Tailwind + Framer-Motion",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Framer Motion",
+    ],
+    github: "https://github.com/zahin-zawad/Foodies",
+    live: "https://foodies-cyan.vercel.app/",
   },
 
   {
     image: image3,
-    title: "Social Media App",
-    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
-    technologies: ["React", "Tailwind CSS", "TypeScript"],
-    github: "#",
-    live: "#"
+    title: "Amazon Clone",
+    description: "Amazon Homepage UI Clone",
+    technologies: ["Vite", "React", "JSX", "CSS", "JavaScript"],
+    github: "https://github.com/zahin-zawad/ecommerce-website-using-react",
+    live: "https://ecommerce-website-using-react-mu.vercel.app/",
   },
 
   {
     image: image4,
-    title: "Restaurant Website",
-    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
-    technologies: ["React", "Tailwind CSS"],
-    github: "#",
-    live: "#"
+    title: "CRUD Admin Dashboard",
+    description:
+      "A simple Dashboard built with React and Tailwind CSS. It allows users to add, delete and edit products",
+    technologies: ["Vite", "React", "Tailwind CSS"],
+    github: "https://github.com/zahin-zawad/CRUD-Dashboard",
+    live: "https://crud-dashboard-sigma.vercel.app/",
   },
 
   {
     image: image5,
-    title: "Admin Dashboard",
-    description: "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    title: "Weather App",
+    description:
+      "Created a weather application with a clean and intuitive user interface to display currentweather and forecasts. Integrated the OpenWeatherMap API for real-time weather data and used Axios for APIrequests.Implemented responsive design principles to ensure compatibility across various devices and screen sizes.",
     technologies: ["React", "Tailwind CSS"],
-    github: "#",
-    live: "#"
+    github: "https://github.com/zahin-zawad/weather-app-using-React",
+    live: "https://weather-app-using-react-seven.vercel.app/",
   },
-]
-
+  {
+    image: image6,
+    title: "Password Generator",
+    description:
+      "A simple Password generator app built with Vite and Tailwind CSS. It allows users generate random password",
+    technologies: ["Vite", "React", "Tailwind CSS"],
+    github: "https://github.com/zahin-zawad/password-generator",
+    live: "https://password-generator-eosin-nine.vercel.app/",
+  },
+  {
+    image: image7,
+    title: "Todo App",
+    description:
+      "A simple todo app built with React and Tailwind CSS. It allows users to add, delete, and mark todos as completed.",
+    technologies: ["Create React App", "JSX", "HTML", "CSS"],
+    github: "https://github.com/zahin-zawad/todo-list-using-React",
+    live: "https://todo-list-using-react-tau.vercel.app/",
+  },
+];
 
 interface Project {
   image: StaticImageData;
@@ -65,48 +98,65 @@ interface Project {
   live: string;
 }
 
-const ProjectCard = ({project}: {project: Project}) => {
+const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className='flex flex-col items-center gap-8 md:flex-row md:gap-24'>
-      <Image src={project.image} alt={project.title} className='w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]' />
+    <div className="flex flex-col items-center gap-8 md:flex-row md:gap-24">
+      <Image
+        src={project.image}
+        alt={project.title}
+        className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]"
+      />
 
-      <div className='flex flex-col gap-5'>
-        <div className='flex flex-col gap-3'>
-          <div className='text-xl font-semibold'>{project.title}</div>
-          <p className='text-gray-400'>{project.description}</p>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-3">
+          <div className="text-xl font-semibold">{project.title}</div>
+          <p className="text-gray-400">{project.description}</p>
         </div>
 
-        <div className='flex flex-wrap gap-5'>
-          {
-            project.technologies.map((tech, index) =>(
-              <span key={index} className='rounded-lg p-3 bg-black'>{tech}</span>
-            ))
-          }
+        <div className="flex flex-wrap gap-5">
+          {project.technologies.map((tech, index) => (
+            <span key={index} className="rounded-lg p-3 bg-black">
+              {tech}
+            </span>
+          ))}
         </div>
 
-        <div className='flex gap-5'>
-        <a href={project.github} className='text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600'>Github</a>
-        <a href={project.live} className='text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600'>Live</a>
+        <div className="flex gap-5">
+          <a
+            href={project.github}
+            target="_blank"
+            className="text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
+          >
+            Github
+          </a>
+          <a
+            href={project.live}
+            target="_blank"
+            className="text-nowrap rounded-lg border border-indigo-600 bg-black text-white px-5 py-3 text-lg font-bold shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
+          >
+            Live
+          </a>
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
 export default function Projects() {
   return (
-    <div id='projects' className='min-h-screen w-full flex flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24'>
-      <h1 className='text-4xl font-light text-white md:text-6xl'>My Projects</h1>
+    <div
+      id="projects"
+      className="min-h-screen w-full flex flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24"
+    >
+      <h1 className="text-4xl font-light text-white md:text-6xl">
+        My Projects
+      </h1>
 
-      <div className='max-w-[1000px] w-full text-white flex flex-col gap-16'>
-        {
-          projectData.map((project, index) =>(
-            <ProjectCard key={index} project={project}/>
-          ))
-        }
+      <div className="max-w-[1000px] w-full text-white flex flex-col gap-16">
+        {projectData.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
